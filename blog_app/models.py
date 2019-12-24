72ca33d181fbd255419d3521928706538502fa97
NuthanChandra <nuthanchandra1997@gmail.com>
2019-12-24 17:32:23 +0530

    End of my implementation

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,7 +1,12 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
 class Post(models.Model):
     author = models.CharField(max_length=256)
     title = models.CharField(max_length=256)
     text = models.TextField()
+
+    def get_absolute_url(self):
+        return reverse("blog_app:blog_detail", kwargs={'pk': self.pk})
