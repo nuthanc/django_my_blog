7d14a2779e1de237d10db9e00038a0cfdcb0502f
NuthanChandra <nuthanchandra1997@gmail.com>
2020-01-26 19:32:19 +0530

    Resolve all errors

--- blog_app/models.py
+++ blog_app/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now())
+    created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
@@ -29,7 +29,7 @@ class Comment(models.Model):
         'blog_app.Post', related_name='comments', on_delete=models.CASCADE)
     author = models.CharField(max_length=200)
     text = models.TextField()
-    create_date = models.DateTimeField(default=timezone.now())
+    created_date = models.DateTimeField(default=timezone.now)
     approved_comment = models.BooleanField(default=False)
 
     def approve(self):
@@ -40,4 +40,4 @@ class Comment(models.Model):
         return self.text
 
     def get_absolute_url(self):
-        return reverse("post_list", kwargs={'pk': self.pk})
+        return reverse("blog_app:post_list", kwargs={'pk': self.pk})
